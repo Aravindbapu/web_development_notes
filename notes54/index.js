d77007f19749bd0e2a713533e0af5290e4d65aca
Kunam Lalith Aravind Bapu <161134004+Aravindbapu@users.noreply.github.com>
2025-04-17 22:48:22 +0530

    HTML and JavaScript

---
+++ notes54/index.js
@@ -0,0 +1,4 @@
+let Aravind_Bapu = document.getElementsByClassName("box");
+console.log(Aravind_Bapu); // Log the HTMLCollection
+
+Aravind_Bapu[2].style.backgroundColor = "red"; // 2 is the third box
