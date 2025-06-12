3100075d3470e004e8457556030c02b0e40a8633
Kunam Lalith Aravind Bapu <161134004+Aravindbapu@users.noreply.github.com>
2025-06-12 19:33:23 +0530

    Introduction to Next.js & File based routing

---
+++ notes82/first/app/about/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-xl'>Problems solved by Next.js</h1>
+      <ul>
+        <li>Full Stack solution</li>
+        <li>File based Routing</li>
+        <li>Additional features like routers from next/navigation</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
